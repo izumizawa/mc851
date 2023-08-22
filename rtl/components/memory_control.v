43cde20d4174a81a13dce7e3c532ac2bab2c7571
Guilherme Tavares Shimamoto <guilherme_shima@hotmail.com>
2023-08-21 21:45:26 -0300

    Adding tests to memory_control

--- rtl/components/memory_control.v
+++ rtl/components/memory_control.v
@@ -1,6 +1,3 @@
-`define ROM_SIZE 1024   
-`define RAM_SIZE 1024   
-
 module memory_control (
 input clk,
 input[31:0] address,
@@ -16,19 +13,19 @@ input[1:0] op_mem,
 input op_if,
 
 // IF should wait (MEM is using)
-output wait_if,
+output reg wait_if,
 // MEM should wait (IF is using)
-output wait_mem,
+output reg wait_mem,
 
 output reg [31:0] data_out
 );
 
 // [mem word size] mem [mem size]
 // 2048(2^11) values of 32 bits
-reg [31:0] mem [0: ROM_SIZE + RAM_SIZE - 1];
+reg [31:0] mem [0: 2047];
 
 always @(posedge clk) begin
-    if (op_mem != 2'b0 ) begin
+    if (op_mem != 2'b0) begin
         wait_if <= 1;
         wait_mem <= 0;
         
@@ -37,13 +34,16 @@ always @(posedge clk) begin
         end else if (op_mem == 2'b10) begin
             mem[address] <= data_in;
         end
-    end else begin
+    end else if (op_if != 1'b0) begin
         wait_mem <= 1;
         wait_if <= 0;
 
         if (op_if == 1'b1) begin
             data_out <= mem[address];
         end
+    end else begin
+        wait_mem <= 0;
+        wait_if <= 0;
     end
 end
 
