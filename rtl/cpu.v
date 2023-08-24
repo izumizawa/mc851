87efea350ddc8b366ac304d4ea8ab3f80e08bfc7
daniel-pg <38893804+daniel-pg@users.noreply.github.com>
2023-08-24 03:25:55 -0300

    Reorganiza código da cpu

--- rtl/cpu.v
+++ rtl/cpu.v
@@ -3,59 +3,29 @@
 module cpu (
     input clk, reset
 );
-    /***************************************************************************
-     * IF Stage Logic
-     **************************************************************************/
-    reg [31:0] pc;
-    @always @(posedge clk) begin
-        ;
-    end
-    // -------------------------------------------------------------------------
-
-
-    // --- IF/ID Register ---
+    // IF/ID Register
     reg [31:0] ifid_pc;
     reg [31:0] ifid_ir;
 
-
-    /***************************************************************************
-     * ID Stage Logic
-     **************************************************************************/
-    @always @(posedge clk) begin
-        ;
-    end
-    // -------------------------------------------------------------------------
-
-
-    // --- ID/EX Register ---
+    // ID/EX Register
     reg [31:0] idex_pc;
-    // Sinais de controle
+    // ... sinais de controle
     reg idex_mem_to_reg;
     reg idex_reg_write;
     reg idex_branch;
     reg idex_mem_read;
     reg idex_mem_write;
-    reg [1:0] idex_alu_src; // TODO: Ajustar conforme implementação da ALU
-    reg [1:0] idex_alu_op;
-    // ALU e Pipeline
+    reg idex_alu_src;
+    reg [ 3:0] idex_alu_op; // TODO: Ajustar conforme implementação da ALU
+    // ... ALU e Pipeline
     reg [31:0] idex_data_read_1;
     reg [31:0] idex_data_read_2;
     reg [ 4:0] idex_rs1;
 	reg [ 4:0] idex_rs2;
 	reg [ 4:0] idex_rd;
-	reg [13:1] idex_imm;
+	reg [12:0] idex_imm;
 
-
-    /***************************************************************************
-     * EX Stage Logic
-     **************************************************************************/
-    @always @(posedge clk) begin
-        ;
-    end
-    // -------------------------------------------------------------------------
-
-
-    // --- EX/MEM Register ---
+    // EX/MEM Register
     reg exmem_mem_to_reg;
     reg exmem_reg_write;
     reg exmem_branch;
@@ -69,26 +39,61 @@ module cpu (
     reg [31:0] exmem_data_read_2;
     reg [ 4:0] exmem_rd;
 
+    // MEM/WB Register
+    reg [31:0] memwb_mem_data_read;
+    reg [31:0] memwb_alu_out;
+    reg [ 4:0] memwb_rd;
+
+    // Control Unit e Hazard Unit
+    reg pc_write;
+    reg ifid_write;
+    // TODO: Gerar sinais de controle ID/EX a partir da instrução decodificada
+
+    // Forwarding Unit
+    // TODO: A fazer.
 
     /***************************************************************************
-     * MEM Stage Logic
+     * Instruction Fetch (IF) stage
      **************************************************************************/
-    @always @(posedge clk) begin
+    reg [31:0] pc;
+
+    always @(posedge clk) begin
         ;
     end
     // -------------------------------------------------------------------------
 
 
-    // --- MEM/WB Register ---
-    reg [31:0] memwb_mem_data_read;
-    reg [31:0] memwb_alu_out;
-    reg [ 4:0] memwb_rd;
+    /***************************************************************************
+     * Instruction Decode (ID) stage
+     **************************************************************************/
+    always @(posedge clk) begin
+        ;
+    end
+    // -------------------------------------------------------------------------
 
 
     /***************************************************************************
-     * WB Stage Logic
+     * Execute (EX) stage
      **************************************************************************/
-    @always @(posedge clk) begin
+    always @(posedge clk) begin
+        ;
+    end
+    // -------------------------------------------------------------------------
+
+
+    /***************************************************************************
+     * Memory access (MEM) stage
+     **************************************************************************/
+    always @(posedge clk) begin
+        ;
+    end
+    // -------------------------------------------------------------------------
+
+
+    /***************************************************************************
+     * Writeback (WB) stage
+     **************************************************************************/
+    always @(posedge clk) begin
         ;
     end
     // -------------------------------------------------------------------------
