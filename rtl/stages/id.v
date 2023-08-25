ba90272a3d626309aebd3490db2a966cb00f742b
Gabriel Braga <gabriel.bragaproenca@gmail.com>
2023-08-24 21:33:50 -0300

    Handle R-type instructions

--- rtl/stages/id.v
+++ rtl/stages/id.v
@@ -1,5 +1,6 @@
 // Instruction decode stage.
 // modules = ["Register File", "Control Unit"]
+`include "define.v"
 
 module RegisterFile (
     input wire clock,          // Sinal de clock
@@ -33,27 +34,61 @@ module Decoder (
     input wire [31:0] instruction, 
 );
     reg [6:0] opcode;
-    reg [4:0] rd;
     reg [2:0] funct3;
-    reg [4:0] rs1;
-    reg [4:0] rs2;
     reg [6:0] funct7;
 
+    output reg [4:0] read_reg1;
+    output reg [4:0] read_reg2;
+    output reg [4:0] write_reg;
+    
     // Read registers
-    assign rs1 = instruction[19:15];
-    assign rs2 = instruction[24:20];
+    assign read_reg1 = instruction[19:15];
+    assign read_reg2 = instruction[24:20];
 
     // Write register
-    assign rd = instruction[11:7];
+    assign write_reg = instruction[11:7];
 
     // Instruction decode
     assign opcode = instruction[6:0];
     assign funct7 = instruction[31:25];
     assign funct3 = instruction[14:12];
 
+    output reg [3:0] alu_input_op;
+
     always @(*) begin 
         case (opcode) 
-            
+            // R-type instructions
+            7'b0110011: begin 
+                write_enable = 1;
+                alu_input_enable = 1;
+
+                if (funct3 == 3'b000) begin 
+                    if (funct7 == 7'b0000000) begin 
+                        alu_input_op = `ALU_ADD;
+                    end else begin 
+                        alu_input_op = `ALU_SUB;
+                    end 
+                end else if (funct3 == 3'b001) begin
+                    alu_input_op = `ALU_SLL; 
+                end else if (funct3 == 3'b010) begin 
+                    alu_input_op = `ALU_SLT;
+                end else if (funct3 == 3'b011) begin
+                    alu_input_op = `ALU_SLTU;
+                end else if (funct3 == 3'b100) begin 
+                    alu_input_op = `ALU_XOR;
+                end else if (funct3 == 3'b101) begin
+                    if (funct7 == 7'b0000000) begin
+                        alu_input_op = `ALU_SRL;
+                    end else begin 
+                        alu_input_op = `ALU_SRA;
+                    end 
+                end else if (funct3 == 3'b110) begin
+                    alu_input_op = `ALU_OR;
+                end else if (funct3 == 3'b111) begin 
+                    alu_input_op = `ALU_AND;
+                end
+            end
+        endcase
     end
 
 endmodule
