1368578558e608494bf4151bfec855aa2f775d28
daniel-pg <--local>
2023-11-03 13:01:23 -0300

    Módulo de ROM e RAM fazem leitura dentro de um ciclo

--- rtl/components/ram.v
+++ rtl/components/ram.v
@@ -1,4 +1,3 @@
-// TODO: implementar teste da RAM
 module ram #(
     parameter ADDR_WIDTH = 8    // 256×4B = 1 KiB
 ) (
@@ -7,18 +6,14 @@ module ram #(
     input read_enable,
     input [ADDR_WIDTH-1:0] address,
     input [31:0] data_in,
-    output reg [31:0] data_out
+    output [31:0] data_out
 );
 
     reg [31:0] mem [0:2**ADDR_WIDTH-1];
 
     always @(posedge clk) begin
-        if (read_enable)
-            data_out <= mem[address];
-        else
-            data_out <= 0;
-
         if (write_enable) mem[address] <= data_in;
     end
 
+    assign data_out = read_enable ? mem[address] : 0;
 endmodule
