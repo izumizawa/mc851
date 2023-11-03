fb61786c19166d78e6e8a01253e426981222573a
Daniel Paulo Garcia <38893804+daniel-pg@users.noreply.github.com>
2023-11-03 18:01:49 -0300

    SLT estava trocado com SLTU

--- rtl/components/alu_module.v
+++ rtl/components/alu_module.v
@@ -1,29 +1,27 @@
 module alu_module (
-    input [3:0]  alu_input_op,
+    input [3:0] alu_op,
     input [31:0] alu_input_a,
     input [31:0] alu_input_b,
-
-    output reg [31:0] alu_output_result
+    output reg [31:0] alu_out
 );
-    always @ (*)
-    begin
-        alu_output_result = 32'h0;
+    always @(*) begin
+        alu_out = 0;
 
-        case (alu_input_op)
+        case (alu_op)
         // ARITHMETIC OPS
-            `ALU_ADD: alu_output_result = (alu_input_a + alu_input_b);
-            `ALU_SUB: alu_output_result = (alu_input_a - alu_input_b);
+            `ALU_ADD: alu_out = (alu_input_a + alu_input_b);
+            `ALU_SUB: alu_out = (alu_input_a - alu_input_b);
         // LOGIC OPS
-            `ALU_AND: alu_output_result = (alu_input_a & alu_input_b);
-            `ALU_OR:  alu_output_result = (alu_input_a | alu_input_b);
-            `ALU_XOR: alu_output_result = (alu_input_a ^ alu_input_b);
+            `ALU_AND: alu_out = (alu_input_a & alu_input_b);
+            `ALU_OR:  alu_out = (alu_input_a | alu_input_b);
+            `ALU_XOR: alu_out = (alu_input_a ^ alu_input_b);
         // SHIFT OPS
-            `ALU_SLL:  alu_output_result = alu_input_a << alu_input_b[4:0]; // need to set a range otherwise it will binary extend the number
-            `ALU_SRL:  alu_output_result = alu_input_a >> alu_input_b[4:0];
-            `ALU_SRA:  alu_output_result = $signed(alu_input_a) >>> alu_input_b[4:0];
+            `ALU_SLL:  alu_out = alu_input_a << alu_input_b[4:0]; // need to set a range otherwise it will binary extend the number
+            `ALU_SRL:  alu_out = alu_input_a >> alu_input_b[4:0];
+            `ALU_SRA:  alu_out = $signed(alu_input_a) >>> alu_input_b[4:0];
         // COMPARE OPS
-            `ALU_SLT:  alu_output_result = (alu_input_a < alu_input_b ? 1 : 0);
-            `ALU_SLTU:  alu_output_result = ($signed(alu_input_a) < $signed(alu_input_b) ? 1 : 0);
+            `ALU_SLT:  alu_out = ($signed(alu_input_a) < $signed(alu_input_b) ? 1 : 0);
+            `ALU_SLTU: alu_out = (alu_input_a < alu_input_b ? 1 : 0);
             default: ;
         endcase
     end
