381c3ef51bb0463b0f9571603f4cfb0b06b096dd
Daniel Paulo Garcia <38893804+daniel-pg@users.noreply.github.com>
2023-11-17 18:01:12 -0300

    Implementa Cache L1 de Instruções

--- rtl/components/l1_instruction_cache.v
+++ rtl/components/l1_instruction_cache.v
@@ -12,20 +12,19 @@ module l1_instruction_cache #(
 );
     localparam TAG_WIDTH        = 32 - (INDEX_WIDTH + OFFSET_WIDTH);
     localparam NUM_OF_BLOCKS    = 2**INDEX_WIDTH;
-    localparam BLOCK_SIZE       = 2**(OFFSET_WIDTH+3); // (bits), = 2**OFFSET_WIDTH bytes
 
     // Cache memory
-    reg [BLOCK_SIZE-1:0] l1_block_data [0:NUM_OF_BLOCKS-1];
+    reg [31:0] l1_block_data [0:NUM_OF_BLOCKS-1];
     reg [TAG_WIDTH-1:0] l1_tag_array [0:NUM_OF_BLOCKS-1];
     reg l1_block_valid [0:NUM_OF_BLOCKS-1];
 
     wire [TAG_WIDTH-1:0]    l1_tag;
     wire [INDEX_WIDTH-1:0]  l1_index;
     wire [OFFSET_WIDTH-1:0] l1_offset;
+
     assign l1_tag       = address[31:INDEX_WIDTH+OFFSET_WIDTH];
     assign l1_index     = address[INDEX_WIDTH+OFFSET_WIDTH-1 : OFFSET_WIDTH];
     assign l1_offset    = address[OFFSET_WIDTH-1:0];
-
 
     initial begin: label0
         integer i;
@@ -36,11 +35,15 @@ module l1_instruction_cache #(
         end
     end
 
-    assign mmu_address = 0;
-    assign cache_miss = (l1_tag_array[l1_index] != l1_tag);
-    assign data_out = l1_block_data[l1_index];
+    assign cache_miss = !l1_block_valid[l1_index] || (l1_tag_array[l1_index] != l1_tag);
+    assign data_out = l1_block_data[l1_index]; // if
+    assign mmu_address = address;
 
     always @(posedge clk) begin
-        ;
+        if (cache_miss && mmu_mem_ready) begin
+            l1_block_data[l1_index] <= mmu_data_out;
+            l1_tag_array[l1_index] <= l1_tag;
+            l1_block_valid[l1_index] <= 1;
+        end
     end
 endmodule
