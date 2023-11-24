4fbf48a0b4d9219f8a12d3d1d27cd85110604fd7
Daniel Paulo Garcia <38893804+daniel-pg@users.noreply.github.com>
2023-11-24 18:18:55 -0300

    Substituir caches por único módulo

--- rtl/components/l1_cache.v
+++ rtl/components/l1_cache.v
@@ -1,15 +1,23 @@
-module l1_instruction_cache #(
-    parameter OFFSET_WIDTH = 2,
+module l1_data_cache #(
     parameter INDEX_WIDTH = 6
 ) (
     input clk, reset_n,
+    input mem_write,
+    input mem_read,
     input [31:0] address,
+    input [31:0] data_in,
     input mmu_mem_ready,
     input [31:0] mmu_data_out,
+    output mmu_write_enable,
+    output mmu_read_enable,
+    output mmu_mem_signed_read,
+    output [ 1:0] mmu_mem_data_width,
+    output [31:0] mmu_data_in,
     output [31:0] mmu_address,
     output cache_miss,
     output [31:0] data_out
 );
+    localparam OFFSET_WIDTH     = 2; // BLOCK_SIZE = 2**OFFSET_WIDTH 4 bytes
     localparam TAG_WIDTH        = 32 - (INDEX_WIDTH + OFFSET_WIDTH);
     localparam NUM_OF_BLOCKS    = 2**INDEX_WIDTH;
 
@@ -17,6 +25,7 @@ module l1_instruction_cache #(
     reg [31:0] l1_block_data [0:NUM_OF_BLOCKS-1];
     reg [TAG_WIDTH-1:0] l1_tag_array [0:NUM_OF_BLOCKS-1];
     reg l1_block_valid [0:NUM_OF_BLOCKS-1];
+    reg l1_block_dirty [0:NUM_OF_BLOCKS-1];
 
     wire [TAG_WIDTH-1:0]    l1_tag;
     wire [INDEX_WIDTH-1:0]  l1_index;
@@ -30,13 +39,26 @@ module l1_instruction_cache #(
         integer i;
         for (i = 0; i < NUM_OF_BLOCKS; i = i + 1) begin
             l1_block_valid[i] = 0;
+            l1_block_dirty[i] = 0;
             l1_tag_array[i] = 0;
             l1_block_data[i] = 0;
         end
     end
 
-    assign cache_miss = !l1_block_valid[l1_index] || (l1_tag_array[l1_index] != l1_tag);
-    assign data_out = l1_block_data[l1_index]; // if
+    assign mmu_write_enable = 0;
+    assign mmu_read_enable = 0;
+    assign mmu_mem_signed_read = 0;
+    assign mmu_mem_data_width = 0;
+    assign mmu_data_in = 0;
+
+    /* Acontece cache miss se:
+     * 1. Ler bloco inválido, ou...
+     * 2. Ler bloco com tag diferente da do endereço
+     * 3. Escrever em bloco sujo && com tag diferente
+     */
+    assign cache_miss = mem_read && (!l1_block_valid[l1_index] || l1_tag_array[l1_index] != l1_tag)
+                    || mem_write && (l1_block_dirty[l1_index] && l1_tag_array[l1_index] != l1_tag);
+    assign data_out = l1_block_data[l1_index];
     assign mmu_address = address;
 
     always @(posedge clk) begin
@@ -46,4 +68,5 @@ module l1_instruction_cache #(
             l1_block_valid[l1_index] <= 1;
         end
     end
+
 endmodule
