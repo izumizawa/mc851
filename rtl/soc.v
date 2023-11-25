2cfabb5397138110345c899bb06f9e3e1a7d7d78
Daniel Paulo Garcia <38893804+daniel-pg@users.noreply.github.com>
2023-11-25 01:07:06 -0300

    Remove código da demo do UART de dentro do SOC
    Instancia uma MMU para cada estágio da CPU

--- rtl/soc.v
+++ rtl/soc.v
@@ -1,128 +1,130 @@
 module soc #(
     parameter ROMFILE="../src/memdump/addi.mem"
 ) (
-    input clk,
-    input reset_n,
-    input btn1,
-    output uart_tx
+    input  wire clk,
+    input  wire button_1,
+    input  wire button_2,
+    input  wire uart_rx,
+    output wire uart_tx
 );
-    wire         mmu_mem_ready;
-    wire [31:0]  mmu_data_out;
-    wire         mmu_write_enable;
-    wire         mmu_read_enable;
-    wire         mmu_mem_signed_read;
-    wire         mmu_signed_read;
-    wire [ 1:0]  mmu_mem_data_width;
-    wire [31:0]  mmu_address;
-    wire [31:0]  mmu_data_in;
+    wire reset_n;
+    assign reset_n = button_1;
 
-    cpu cpu_inst (
+    // Instruction MMU
+    wire        immu_mem_ready;
+    wire [31:0] immu_data_out;
+    wire        immu_write_enable;
+    wire        immu_read_enable;
+    wire        immu_signed_read;
+    wire [ 1:0] immu_data_width;
+    wire [31:0] immu_address;
+    wire [31:0] immu_data_in;
+    //--------------------------
+    wire        immu_imc_mem_ready;
+    wire [31:0] immu_imc_data_out;
+    wire        immu_imc_write_enable;
+    wire        immu_imc_read_enable;
+    wire [31:0] immu_imc_address;
+    wire [31:0] immu_imc_data_in;
+    //--------------------------
+    wire        immu_l1_cache_miss;
+    wire [31:0] immu_l1_data_out;
+    wire        immu_l1_write_enable;
+    wire        immu_l1_read_enable;
+    wire [31:0] immu_l1_address;
+    wire [31:0] immu_l1_data_in;
+    //--------------------------//
+
+    // Data MMU
+    wire        dmmu_mem_ready;
+    wire [31:0] dmmu_data_out;
+    wire        dmmu_write_enable;
+    wire        dmmu_read_enable;
+    wire        dmmu_signed_read;
+    wire [ 1:0] dmmu_data_width;
+    wire [31:0] dmmu_address;
+    wire [31:0] dmmu_data_in;
+    //--------------------------
+    wire        dmmu_imc_mem_ready;
+    wire [31:0] dmmu_imc_data_out;
+    wire        dmmu_imc_write_enable;
+    wire        dmmu_imc_read_enable;
+    wire [31:0] dmmu_imc_address;
+    wire [31:0] dmmu_imc_data_in;
+    //--------------------------
+    wire        dmmu_l1_cache_miss;
+    wire [31:0] dmmu_l1_data_out;
+    wire        dmmu_l1_write_enable;
+    wire        dmmu_l1_read_enable;
+    wire [31:0] dmmu_l1_address;
+    wire [31:0] dmmu_l1_data_in;
+    //--------------------------//
+
+    cpu cpu_core0 (
         .clk (clk),
         .reset_n (reset_n),
-        .mmu_mem_ready(mmu_mem_ready),
-        .mmu_data_out(mmu_data_out),
-        .mmu_write_enable(mmu_write_enable),
-        .mmu_read_enable(mmu_read_enable),
-        .mmu_mem_signed_read(mmu_mem_signed_read),
-        .mmu_mem_data_width(mmu_mem_data_width),
-        .mmu_address(mmu_address),
-        .mmu_data_in(mmu_data_in)
+
+        .immu_mem_ready(immu_mem_ready),
+        .immu_data_out(immu_data_out),
+        .immu_write_enable(immu_write_enable),
+        .immu_read_enable(immu_read_enable),
+        .immu_signed_read(immu_signed_read),
+        .immu_data_width(immu_data_width),
+        .immu_address(immu_address),
+        .immu_data_in(immu_data_in),
+
+        .dmmu_mem_ready(dmmu_mem_ready),
+        .dmmu_data_out(dmmu_data_out),
+        .dmmu_write_enable(dmmu_write_enable),
+        .dmmu_read_enable(dmmu_read_enable),
+        .dmmu_signed_read(dmmu_signed_read),
+        .dmmu_data_width(dmmu_data_width),
+        .dmmu_address(dmmu_address),
+        .dmmu_data_in(dmmu_data_in)
     );
 
-    mmu #( .ROMFILE(ROMFILE)) mmu_inst (
+    mmu instruction_mmu (
         .clk(clk),
         .reset_n(reset_n),
-        .write_enable(mmu_write_enable),
-        .read_enable(mmu_read_enable),
-        .mem_signed_read(mmu_mem_signed_read),
-        .mem_data_width(mmu_mem_data_width),
-        .address(mmu_address),
-        .data_in(mmu_data_in),
-        .data_out(mmu_data_out),
-        .mem_ready(mmu_mem_ready)
+        .write_enable(immu_write_enable),
+        .read_enable(immu_read_enable),
+        .signed_read(immu_signed_read),
+        .data_width(immu_data_width),
+        .address(immu_address),
+        .data_in(immu_data_in),
+        .mem_ready(immu_mem_ready),
+        .data_out(immu_data_out),
+
+        .imc_mem_ready(immu_imc_mem_ready),
+        .imc_data_out(immu_imc_data_out),
+        .imc_write_enable(immu_imc_write_enable),
+        .imc_read_enable(immu_imc_read_enable),
+        .imc_address(immu_imc_address),
+        .imc_data_in(immu_imc_data_in)
     );
 
-// TEST
-reg [3:0] txState = 0;
-reg [24:0] txCounter = 0;
-reg [7:0] dataOut = 0;
-reg [7:0] dataArray [0:3];
-reg txPinRegister = 1;
-reg [2:0] txBitNumber = 0;
-reg [1:0] txByteCounter = 0;
-wire [31:0] data;
+    mmu data_mmu (
+        .clk(clk),
+        .reset_n(reset_n),
+        .write_enable(dmmu_write_enable),
+        .read_enable(dmmu_read_enable),
+        .signed_read(dmmu_signed_read),
+        .data_width(dmmu_data_width),
+        .address(dmmu_address),
+        .data_in(dmmu_data_in),
+        .mem_ready(dmmu_mem_ready),
+        .data_out(dmmu_data_out),
 
-// assign data = 32'b11111001101110011001101110011111;
-assign uart_tx = txPinRegister;
+        .imc_mem_ready(dmmu_imc_mem_ready),
+        .imc_data_out(dmmu_imc_data_out),
+        .imc_write_enable(dmmu_imc_write_enable),
+        .imc_read_enable(dmmu_imc_read_enable),
+        .imc_address(dmmu_imc_address),
+        .imc_data_in(dmmu_imc_data_in)
+    );
 
-localparam TX_STATE_IDLE = 0;
-localparam TX_STATE_START_BIT = 1;
-localparam TX_STATE_WRITE = 2;
-localparam TX_STATE_STOP_BIT = 3;
-localparam TX_STATE_DEBOUNCE = 4;
+    //TODO: Instanciar Memory Controller (IMC)
 
-localparam DELAY_FRAMES = 234; // 27,000,000 (27Mhz) / 115200 Baud rate
-
-always @(posedge clk) begin
-    case (txState)
-        TX_STATE_IDLE: begin
-            if (btn1 == 0) begin
-                txState <= TX_STATE_START_BIT;
-                txCounter <= 0;
-                txByteCounter <= 0;
-                dataArray[0] <= data[31:24];
-                dataArray[1] <= data[23:16];
-                dataArray[2] <= data[15:8];
-                dataArray[3] <= data[7:0];
-            end
-            else begin
-                txPinRegister <= 1;
-            end
-        end
-        TX_STATE_START_BIT: begin
-            txPinRegister <= 0;
-            if ((txCounter + 1) == DELAY_FRAMES) begin
-                txState <= TX_STATE_WRITE;
-                dataOut <= dataArray[txByteCounter];
-                txBitNumber <= 0;
-                txCounter <= 0;
-            end else
-                txCounter <= txCounter + 1;
-        end
-        TX_STATE_WRITE: begin
-            txPinRegister <= dataOut[txBitNumber];
-            if ((txCounter + 1) == DELAY_FRAMES) begin
-                if (txBitNumber == 3'b111) begin
-                    txState <= TX_STATE_STOP_BIT;
-                end else begin
-                    txState <= TX_STATE_WRITE;
-                    txBitNumber <= txBitNumber + 1;
-                end
-                txCounter <= 0;
-            end else
-                txCounter <= txCounter + 1;
-        end
-        TX_STATE_STOP_BIT: begin
-            txPinRegister <= 1;
-            if ((txCounter + 1) == DELAY_FRAMES) begin
-                if (txByteCounter == 2'b11) begin
-                    txState <= TX_STATE_DEBOUNCE;
-                end else begin
-                    txByteCounter <= txByteCounter + 1;
-                    txState <= TX_STATE_START_BIT;
-                end
-                txCounter <= 0;
-            end else
-                txCounter <= txCounter + 1;
-        end
-        TX_STATE_DEBOUNCE: begin
-            if (txCounter == 25'b11111111111111111111) begin
-                if (btn1 == 1)
-                    txState <= TX_STATE_IDLE;
-            end else
-                txCounter <= txCounter + 1;
-        end
-    endcase
-end
+    //TODO: Instanciar pROM, Semi dual-port B-SRAM, User Flash, etc...
 
 endmodule
