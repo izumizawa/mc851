27f5e5a531a6e2ebd6233ca1d42bc76b924a69a1
Guilherme Tavares Shimamoto <guilherme_shima@hotmail.com>
2023-11-27 21:47:54 -0300

    Adding gowin EDA project

---
+++ mc851_gowin_project/src/gowin_sp/ram_gowin_sp.mod
@@ -0,0 +1,17 @@
+-series GW1NR
+-device GW1NR-9
+-device_version C
+-package QFN88P
+-part_number GW1NR-LV9QN88PC6/I5
+
+
+-mod_name Gowin_SP
+-file_name ram_gowin_sp
+-path C:/Users/Guilherme/Desktop/faculdade/mc851/mc851_gowin/mc851_gowin_project/src/gowin_sp/
+-type RAM_SP
+-file_type vlg
+-depth 32
+-width 32
+-read_mode bypass
+-write_mode normal
+-reset_mode sync
---
+++ mc851_gowin_project/src/ram.v
@@ -0,0 +1,24 @@
+// TODO: implementar teste da RAM
+module ram #(
+    parameter ADDR_WIDTH = 8    // 256×4B = 1 KiB
+) (
+    input clk,
+    input write_enable,
+    input read_enable,
+    input [ADDR_WIDTH-1:0] address,
+    input [31:0] data_in,
+    output reg [31:0] data_out
+);
+
+    reg [31:0] mem [0:2**ADDR_WIDTH-1]/* synthesis syn_ramstyle = "block_ram" */;
+
+    always @(posedge clk) begin
+        if (read_enable)
+            data_out <= mem[address];
+        else
+            data_out <= 0;
+
+        if (write_enable) mem[address] <= data_in;
+    end
+
+endmodule
